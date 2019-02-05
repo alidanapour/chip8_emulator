26c5c20a6a794a8b6e535cf5c322e9febacf12d4
Karan Pathania <33276528+karancodes@users.noreply.github.com>
2019-02-05 01:48:59 -0800

    Update and rename myScreen.js to screen.js

--- chip-8-webpage/scripts/screen.js
+++ chip-8-webpage/scripts/screen.js
@@ -32,52 +32,51 @@ function run() {
     // window.requestAnimationFrame(renderScreen());
 } */
 
-let screenWidth = 64;
-let screenHeight = 32;
-let numberOfPixels = screenWidth * screenHeight;
+let sWidth = 64;
+let sHeight = 32;
+let numberOfPixels = sWidth * sHeight;
 
 let running = true;
 
 let display = new Array(numberOfPixels);
 display.fill(1);
+display[2] = 1;
 
 let tempArray= new Array(numberOfPixels);
 tempArray.fill(1);
+display[2] = 1;
 
-let i = 0;
-function renderScreen() {
-    const canvas = document.getElementById("myScreen");
-    const screen = canvas.getContext("2d");
+////////////////////////////////////////////////////////
 
-    for (let y = 0; y < screenHeight; y++) {
-        for (let x = 0; x < screenWidth; x++) {
-            if (display[x + screenWidth * y] == 1)
-                screen.fillRect (x * 10, y * 10, 10, 10);
-			else
-				screen.clearRect(x * 10, y * 10, 10, 10)
-        }
-
-    }
-
-    display=xor(display, tempArray)
-}
-
-function xor(a1, a2){
-	let newArray= new Array(a1.length);
+function xorArrays(array1, array2) {
+	let newArray= new Array(array1.length);
 	newArray.fill(0);
 
-	let i=0;
-	let j=0;
-	let length= a1.length;
-
-	for(i;i<length;i++){
-		if(a1[i]==a2[i]){
-			newArray[i]=0;
-		}
-
-		else if(a1[i]!=a2[i]){
-			newArray[i]=1;
-		}
+	let length = array1.length; 
+	for(let i = 0; i < length; i++){
+		if (array1[i] === array2[i])
+			newArray[i] = 0;
+		else
+			newArray[i] = 1;
 	}
 	return newArray;
+}
+
+////////////////////////////////////////////////////////
+
+function renderScreen() {
+    const canvas = document.getElementById("screen-area");
+    const screen = canvas.getContext("2d");
+
+    for (let y = 0; y < sHeight; y++) {
+        for (let x = 0; x < sWidth; x++) {
+            if (display[x + sWidth * y] == 1)
+                screen.fillRect (x * 10, y * 10, 10, 10);
+			else
+				screen.clearRect(x * 10, y * 10, 10, 10);
+        }
+    }
+
+    display = xorArrays(display, tempArray);
+	// window.requestAnimationFrame(renderScreen);
 }
