5778b2b337268b139fe2a66f1f98ee98a9cc1992
Karan Pathania <33276528+karancodes@users.noreply.github.com>
2019-02-06 22:54:34 -0800

    Update chipCpu.js

--- chip-8-webpage/scripts/chipCpu.js
+++ chip-8-webpage/scripts/chipCpu.js
@@ -1,57 +1,56 @@
 let CHIP8 = {
-    V: new Uint8Array(16),          // V0 to Vf are 16 8-bit registers
-    I: 0,                  			// Special 16-bit address register I
-    
-    memory: new Uint8Array(4096),   // 4K (4096) bytes of memory
+	V: new Uint8Array(16),          // V0 to Vf are 16 8-bit registers
+	I: 0,                  			// Special 16-bit address register I
+
+	memory: new Uint8Array(4096),   // 4K (4096) bytes of memory
 	PC: 0x200,                      // Program counter (16-bit) starts at 0x200 (512)
 
-    stack: new Uint16Array(16),     // Array of 16 16-bit values
-    stackPointer: 0,               	// Points to the top of the stack
+	stack: new Uint16Array(16),     // Array of 16 16-bit values
+	stackPointer: 0,               	// Points to the top of the stack
 
-    delayTimer: 0,                  // Delay timer initialized to 0
+	delayTimer: 0,                  // Delay timer initialized to 0
 	soundTimer: 0,                  // Sound timer initialized to 0
-	
-    isRunning: false,               // CPU run status
+
+	isRunning: false,               // CPU run status
 	programLoaded: false,			// True when the program has been loaded into memory
 	keys: new Uint8Array(16),		// Stores the status of the keys
 	keyPressed: false,				// True when a key is pressed
-    
+
 	screenWidth: 64,
 	screenHeight: 32,
-	display: new Uint8Array(64*32),	// Video memory, used to draw frames
+	display: new Uint8Array(64 * 32),	// Video memory, used to draw frames
 	drawFlag: false,				// Tells whether to draw
 	upT: null,						// Interval for updating timers
+	fontsToStore: [
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	],
 
 	////////
 	// INTERVALS: null,
 	///////
-	
+
 	// Load font set to memory
-    loadFonts: function() {
-        let fontsToStore = [
-            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-            0x20, 0x60, 0x20, 0x20, 0x70, // 1
-            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-			0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-		];
-        
-        let length = fontsToStore.length;
-        for (let i = 0; i < length; i++) {
-            this.memory[i] = fontsToStore[i];
-        }
-    }
+	loadFonts: function () {
+		let length = this.fontsToStore.length;
+		for (let i = 0; i < length; i++) {
+			this.memory[i] = this.fontsToStore[i];
+		}
+	}
 };
 
 // Reset CPU state
@@ -70,7 +69,7 @@ function reset() {
 	CHIP8.keys.fill(0);			// Clear keys
 	CHIP8.keyPressed = false;	// Set key pressed to false
 	CHIP8.drawFlag = false;		// Don't draw anything
-	this.upT = setInterval(this.updateTimers, 100);
+	CHIP8.upT = setInterval(this.updateTimers, 100);
 
 
 	// //
@@ -106,28 +105,27 @@ function reset() {
 //////////////////////////////
 
 // Convert to HEX string (base - 16)
-function convertToHexString (opcode)
-{
+function convertToHexString(opcode) {
 	let hexArray = (opcode).toString(16).toUpperCase();
 	let addLength = 4 - hexArray.length;
 	let padding = "";
 	for (let i = 0; i < addLength; i++)
 		padding += "0";
 	let finalString = "0x" + padding + hexArray;
-  	return finalString;
+	return finalString;
 }
 
 // Opcode implementations
-function emulateOpcode (opcode) {
+function emulateOpcode(opcode) {
 	let code = opcode; 						// 1111-2222-3333-4444
-    let x = (code & 0x0F00) >>> 8;          // 0x00
-    let y = (code & 0x00F0) >>> 4;          // 00y0
-    let kk = code & 0x00FF;                 // 00kk
-    let nnn = code & 0xFFF;					// 0nnn
+	let x = (code & 0x0F00) >>> 8;          // 0x00
+	let y = (code & 0x00F0) >>> 4;          // 00y0
+	let kk = code & 0x00FF;                 // 00kk
+	let nnn = code & 0xFFF;					// 0nnn
 
 	switch (code & 0xF000) {
 		case 0x0000:
-			
+
 			switch (code & 0x00FF) {
 
 				case 0x00E0:
@@ -236,33 +234,33 @@ function emulateOpcode (opcode) {
 				case 0x0004:
 
 					// 8xy4 - ADD Vx, Vy
-                    let sum = CHIP8.V[x] + CHIP8.V[y];
-                    if (sum > 0xff)
-                        CHIP8.V[15] = 1;
-                    else
-                        CHIP8.V[15] = 0;
-                    CHIP8.V[x] = sum;
+					let sum = CHIP8.V[x] + CHIP8.V[y];
+					if (sum > 0xFF)
+						CHIP8.V[15] = 1;
+					else
+						CHIP8.V[15] = 0;
+					CHIP8.V[x] = sum & 0xFF;
 					break;
 
 				case 0x0005:
 
 					// 8xy5 - SUB Vx, Vy
 					if (CHIP8.V[x] > CHIP8.V[y])
-                        CHIP8.V[15] = 1;           // Vf = Not Borrow
-                    else
-                        CHIP8.V[15] = 0;
-                    CHIP8.V[x] -= CHIP8.V[y];
+						CHIP8.V[15] = 1;           // Vf = Not Borrow
+					else
+						CHIP8.V[15] = 0;
+					CHIP8.V[x] -= CHIP8.V[y];
 					CHIP8.PC += 2;
 					break;
 
 				case 0x0006:
 
-                    // 8xy6 - SHR Vx {, Vy}
-                    if (CHIP8.V[x] & 0x01)
-                        CHIP8.V[15] = 1;
-                    else
-                        CHIP8.V[15] = 0;
-                    CHIP8.V[x] = CHIP8.V[x] >>> 1;    // >> or >>>
+					// 8xy6 - SHR Vx {, Vy}
+					if (CHIP8.V[x] & 0x01)
+						CHIP8.V[15] = 1;
+					else
+						CHIP8.V[15] = 0;
+					CHIP8.V[x] = CHIP8.V[x] >>> 1;    // >> or >>>
 					CHIP8.PC += 2;
 					break;
 
@@ -270,25 +268,24 @@ function emulateOpcode (opcode) {
 
 					// 8xy7 - SUBN Vx, Vy
 					if (CHIP8.V[y] > CHIP8.V[x])	    // Vf = Not Borrow
-                        CHIP8.V[15] = 1;
-                    else
-                        CHIP8.V[15] = 0;
-                    CHIP8.V[x] = CHIP8.V[y] - CHIP8.V[x];
+						CHIP8.V[15] = 1;
+					else
+						CHIP8.V[15] = 0;
+					CHIP8.V[x] = CHIP8.V[y] - CHIP8.V[x];
 					CHIP8.PC += 2;
 					break;
 
 				case 0x000E:
 
-                    // 8xyE - SHL Vx {, Vy}
-                    if (CHIP8.V[x] & 0x80)
-                        CHIP8.V[15] = 1;
-                    else
-                        CHIP8.V[15] = 0;
-                    CHIP8.V[x] = CHIP8.V[x] << 1;
-                    break;
-            
+					// 8xyE - SHL Vx {, Vy}
+					if (CHIP8.V[x] & 0x80)
+						CHIP8.V[15] = 1;
+					else
+						CHIP8.V[15] = 0;
+					CHIP8.V[x] = CHIP8.V[x] << 1;
+					break;
 			}
-		    break;
+			break;
 
 		case 0x9000:
 
@@ -380,64 +377,64 @@ function emulateOpcode (opcode) {
 				case 0x0015:
 
 					// Fx15 - LD DT, Vx
-                    CHIP8.delayTimer = CHIP8.V[x];
-                    CHIP8.PC += 2;
+					CHIP8.delayTimer = CHIP8.V[x];
+					CHIP8.PC += 2;
 					break;
 
 				case 0x0018:
 
 					// Fx18 - LD ST, Vx
-                    CHIP8.soundTimer = CHIP8.V[x];
-                    CHIP8.PC += 2;
+					CHIP8.soundTimer = CHIP8.V[x];
+					CHIP8.PC += 2;
 					break;
 
 				case 0x001E:
 
-                    // Fx1E - ADD I, Vx
-                    CHIP8.I += CHIP8.V[x];
-                    CHIP8.PC += 2;
+					// Fx1E - ADD I, Vx
+					CHIP8.I += CHIP8.V[x];
+					CHIP8.PC += 2;
 					break;
 
 				case 0x0029:
 
-                    // Fx29 - LD F, Vx
-                    CHIP8.I = CHIP8.V[x] * 0x5;
-                    CHIP8.PC += 2;
+					// Fx29 - LD F, Vx
+					CHIP8.I = CHIP8.V[x] * 0x5;
+					CHIP8.PC += 2;
 					break;
 
 				case 0x0033:
 
 					// Fx33 - LD B, Vx
-					CHIP8.memory[CHIP8.I]     = CHIP8.V[x] / 100;
+					CHIP8.memory[CHIP8.I] = CHIP8.V[x] / 100;
 					CHIP8.memory[CHIP8.I + 1] = (CHIP8.V[x] / 10) % 10;
 					CHIP8.memory[CHIP8.I + 2] = (CHIP8.V[x] % 100) % 10;
 					break;
 
 				case 0x0055:
 
-                    // Fx55 - LD [I], Vx
-                    for (let i = 0; i <= x; i++) {
-                        CHIP8.memory[CHIP8.I + i] = CHIP8.V[i];
+					// Fx55 - LD [I], Vx
+					for (let i = 0; i <= x; i++) {
+						CHIP8.memory[CHIP8.I + i] = CHIP8.V[i];
 					}
 					CHIP8.I += x + 1;
-                    CHIP8.PC += 2;
+					CHIP8.PC += 2;
 					break;
 
 				case 0x0065:
 
-                    // Fx65 - LD Vx, [I]
-                    for (let i = 0; i <= x; i++) {
-                        CHIP8.V[i] = CHIP8.memory[CHIP8.I + i];
+					// Fx65 - LD Vx, [I]
+					for (let i = 0; i <= x; i++) {
+						CHIP8.V[i] = CHIP8.memory[CHIP8.I + i];
 					}
 					CHIP8.I += x + 1;
-                    CHIP8.PC += 2;
+					CHIP8.PC += 2;
 					break;
 
 			}
 
-            break;
-            // no default case (is it needed?)
-    }
+			break;
+		// no default case (is it needed?)
+	}
 }
 
 function updateTimers() {
@@ -450,16 +447,16 @@ function updateTimers() {
 // Main run cycle
 function run() {
 	let opcode = CHIP8.memory[CHIP8.PC] << 8 | CHIP8.memory[CHIP8.PC + 1];
-	let op = emulateOpcode(convertToHexString(opcode));
-	while(1)
-		op = op(opcode);
+	updateTimers();
+	emulateOpcode(opcode);
+	renderScreen();
 }
 
 // Renders the display onto the screen by XORing
 function renderScreen() {
 	if (CHIP8.drawFlag === false)
 		return;
-	
+
 	const canvas = document.getElementById("screen-area")
 	const screen = canvas.getContext("2d");
 
@@ -467,17 +464,17 @@ function renderScreen() {
 
 	for (let i = 0; i < length; i++) {
 		if (CHIP8.display[i] === 1) {
-			let y = i/64 | 0;
-			let x = i - (y*64);
-			screen.fillRect(x*10, y*10, 10, 10);
+			let y = i / 64 | 0;
+			let x = i - (y * 64);
+			screen.fillRect(x * 10, y * 10, 10, 10);
 		}
 		else if (CHIP8.display[i] === 0) {
-			let y = i/64 | 0;
-			let x = i - (y*64);
-			screen.clearRect(x*10, y*10, 10, 10);
+			let y = i / 64 | 0;
+			let x = i - (y * 64);
+			screen.clearRect(x * 10, y * 10, 10, 10);
 		}
 	}
-	
+
 	CHIP8.drawFlag = false;
 	// requestAnimationFrame(renderScreen);
 }
