5f881705b54872be68b33b88d25e2a8fe3472aa8
JJ <jjl45@sfu.ca>
2019-02-18 20:37:40 -0800

    Updated more test scripts
    CHANGES DONE SO FAR:
    - Added Node.js + Jest
    - Did not touch test.js but added a segment to cpu.js and added cpu.test.js
    - To run JEST: Install Node.js, go to scripts directory and run: "npm run test"
    
    TODO:
    - Finish test cases (for cpu.js also other files)
    - Add it to a more recent release

--- chip8-webpage/scripts/cpu.test.js
+++ chip8-webpage/scripts/cpu.test.js
@@ -1,10 +1,30 @@
 var functions = require('./cpu');
 
-test( 'Array V is it empty ', ()=>{
-	expect(functions.VIsEmpty()).toBe(true);
-});
-
-test( 'Length of Array V should be 16 ', ()=>{
+test( 'Length of Array V is 16 ', ()=>{
 	expect(functions.lengthOfArrayV()).toBe(16);
 });
 
+test( 'Array V is empty ', ()=>{
+	expect(functions.vIsEmpty()).toBe(true);
+});
+
+test( 'I is 0 ', ()=>{
+	expect(functions.iIsEmpty()).toBe(0);
+});
+
+test( 'Memory is 4096 bytes wide ', ()=>{
+	expect(functions.memoryLength()).toBe(4096);
+});
+
+
+test( 'Memory is empty ', ()=>{
+	expect(functions.memoryIsEmpty()).toBe(true);
+});
+
+test( 'Program Counter starts at 0x200 ', ()=>{
+	expect(functions.pcValue()).toBe(0x200);
+});
+
+test( 'Stack length is 16 ', ()=>{
+	expect(functions.stackLength()).toBe(16);
+});
