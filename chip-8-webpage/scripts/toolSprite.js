6d544b0a9bf62a58e3333b11778cb68944051b1d
tmbui <tmbui@sfu.ca>
2019-03-13 22:23:20 -0700

    reformat code in toolSprite.js and style.css

--- chip-8-webpage/scripts/toolSprite.js
+++ chip-8-webpage/scripts/toolSprite.js
@@ -1,27 +1,43 @@
-var lastClicked;
 
-var grid = clickableGrid(30,16,function(el){
-    el.className='clicked';
-    lastClicked = el;
-});
-
-var container = document.getElementById("sprite-draw-area");
-container.appendChild(grid);
-     
 function clickableGrid( rows, cols, callback ){
+
     var i=0;
     var grid = document.createElement('table');
     grid.className = 'grid';
-    for (var r=0;r<rows;++r){
+
+    for (var r = 0; r < rows; r++){
+
+        // add rows
         var tr = grid.appendChild(document.createElement('tr'));
-        for (var c=0;c<cols;++c){
+
+        for (var c = 0; c < cols; c++){
+
+            // add cells 
             var cell = tr.appendChild(document.createElement('td'));
-            cell.addEventListener('click',(function(el,r,c,i){
+
+            // add event listener to each cell 
+            cell.addEventListener('click', (function(el, r, c, i){
+
                 return function(){
-                    callback(el,r,c,i);
+
+                    callback(el, r, c, i);
+
                 }
-            })(cell,r,c,i),false);
+
+            })(cell, r, c, i),false);
+        
         }
+
     }
+
     return grid;
 }
+
+// create grid element 
+var grid = clickableGrid(30, 16, function(el){
+    el.className='clicked';
+});
+
+// add grid inside the div with id sprite-draw-area
+var container = document.getElementById("sprite-draw-area");
+container.appendChild(grid);
