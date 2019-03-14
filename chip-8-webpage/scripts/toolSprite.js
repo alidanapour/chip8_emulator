9e0f96d84a4eebfd2ca226973ec23e9ea9100e46
tmbui <tmbui@sfu.ca>
2019-03-13 21:51:47 -0700

    added tool

---
+++ chip-8-webpage/scripts/toolSprite.js
@@ -0,0 +1,27 @@
+var lastClicked;
+
+var grid = clickableGrid(30,16,function(el){
+    el.className='clicked';
+    lastClicked = el;
+});
+
+var container = document.getElementById("sprite-draw-area");
+container.appendChild(grid);
+     
+function clickableGrid( rows, cols, callback ){
+    var i=0;
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var r=0;r<rows;++r){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var cell = tr.appendChild(document.createElement('td'));
+            cell.addEventListener('click',(function(el,r,c,i){
+                return function(){
+                    callback(el,r,c,i);
+                }
+            })(cell,r,c,i),false);
+        }
+    }
+    return grid;
+}
