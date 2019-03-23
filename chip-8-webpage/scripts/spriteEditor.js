5cbb01bb0691f8b5ff5f2a4f4a2432eb3f9a7136
Karan Pathania <karanpathania98@gmail.com>
2019-03-22 23:39:52 -0700

    Fixed sprite tool drawing events

--- chip-8-webpage/scripts/spriteEditor.js
+++ chip-8-webpage/scripts/spriteEditor.js
@@ -1,25 +1,38 @@
 let scalingFactor = 20;         		// Scale pixels by this factor
 
-let canvas = document.getElementById('sprite-draw')
-let c = canvas.getContext('2d');
+let spriteEditorArea = document.getElementById('sprite-draw');
 let spriteLength = 15;
 let spriteWidth = 8;
 let numberOfPixels = spriteLength * spriteWidth;
 
-canvas.addEventListener('click', updateSpriteArea);
-canvas.addEventListener('mousemove', updateSpriteArea)
+spriteEditorArea.onmousedown = updateSpriteArea;
+spriteEditorArea.onmousemove = updateSpriteArea;
+spriteEditorArea.oncontextmenu = () => false;   // Disable right click within the canvas
 
 let pixelArray = new Array(numberOfPixels);     // Array to store sprite pixels
 
 function clearSpriteArea() {
-    c.fillStyle = 'lightslategrey';
-    c.fillRect(0, 0, 300, 160);
+    const ctx = spriteEditorArea.getContext('2d');
+    ctx.fillStyle = 'lightslategrey';
+    ctx.fillRect(0, 0, spriteEditorArea.clientWidth, spriteEditorArea.height);
 }
 
 function updateSpriteArea(e) {
-    c.fillStyle = 'black';
-    c.fillRect(Math.floor(e.offsetX / scalingFactor) * scalingFactor,
-               Math.floor(e.offsetY / scalingFactor) * scalingFactor,
-               scalingFactor,
-               scalingFactor);
+    if (e.buttons == 1) {
+        const ctx = spriteEditorArea.getContext('2d');
+        ctx.fillStyle = 'black';
+        ctx.fillRect(Math.floor(e.offsetX / scalingFactor) * scalingFactor,
+                    Math.floor(e.offsetY / scalingFactor) * scalingFactor,
+                    scalingFactor,
+                    scalingFactor);
+    }
+
+    else if (e.buttons == 2) {
+        const ctx = spriteEditorArea.getContext('2d');
+        ctx.fillStyle = 'lightslategrey';
+        ctx.fillRect(Math.floor(e.offsetX / scalingFactor) * scalingFactor,
+                    Math.floor(e.offsetY / scalingFactor) * scalingFactor,
+                    scalingFactor,
+                    scalingFactor);
+    }
 }
