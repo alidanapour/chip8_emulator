171e7591200b2836cac231203d7183a9ed2c9781
Karan Pathania <33276528+karancodes@users.noreply.github.com>
2019-03-29 19:01:16 -0700

    Adding moving features to the tool

--- chip-8-webpage/scripts/spriteEditor.js
+++ chip-8-webpage/scripts/spriteEditor.js
@@ -1,16 +1,17 @@
-let scalingFactor = 20;         		// Scale pixels by this factor
+const scalingFactor = 20;         		// Scale pixels by this factor
 
-let spriteEditorArea = document.getElementById('sprite-draw');
-let spriteLength = 15;
-let spriteWidth = 8;
+const spriteEditorArea = document.getElementById('sprite-draw');
+const spriteLength = 15;
+const spriteWidth = 8;
 let numberOfPixels = spriteLength * spriteWidth;
-let spriteText = document.getElementById('sprite-output');
-spriteText.value = "0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00";
+
+const spriteTextBox = document.getElementById('sprite-output');
+spriteTextBox.value = "0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00";
 
 spriteEditorArea.onmousedown = updateSpriteArea;
 spriteEditorArea.onmousemove = updateSpriteArea;
 spriteEditorArea.oncontextmenu = () => false;       // Disable right click within the sprite editor area
-spriteText.oninput = renderSpriteScreenFromText;
+spriteTextBox.oninput = renderSpriteScreenFromText;
 
 let pixelArray = [0x00, 0x00, 0x00, 0x00, 0x00,
                   0x00, 0x00, 0x00, 0x00, 0x00, 
@@ -27,20 +28,56 @@ function clearSpriteArea() {
     const ctx = spriteEditorArea.getContext('2d');
     ctx.fillStyle = '#c4c4c4';
     ctx.fillRect(0, 0, spriteEditorArea.width, spriteEditorArea.height);
-    spriteText.value = "0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00";
+    spriteTextBox.value = "0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00";
     pixelArray = [0x00, 0x00, 0x00, 0x00, 0x00, 
                   0x00, 0x00, 0x00, 0x00, 0x00, 
                   0x00, 0x00, 0x00, 0x00, 0x00];
 }
 
+function moveSpriteUp() {
+    const tempTopByte = pixelArray[0];                // Store the top most line
+    pixelArray.shift();                             // Remove the top most line
+    pixelArray.push(tempTopByte);                   // Push the line stored to the end
+    spriteTextBox.value = toHexString(pixelArray);
+    renderSpriteScreenFromText();
+}
+
+function moveSpriteDown() {
+    const tempBottomByte = pixelArray.pop();          // Remove and store the bottom line
+    pixelArray.unshift(tempBottomByte);             // Push the line stored to the front
+    spriteTextBox.value = toHexString(pixelArray);
+    renderSpriteScreenFromText();
+}
+
+function moveSpriteLeft() {
+    for (let i = 0; i < pixelArray.length; i++) {
+        let currentByte = pixelArray[i];
+        let firstBit = (currentByte & 0b10000000) ? 1 : 0;
+        pixelArray[i] = ((currentByte << 1) & 0xFF) | firstBit;
+    }
+    spriteTextBox.value = toHexString(pixelArray);
+    renderSpriteScreenFromText();
+}
+
+function moveSpriteRight() {
+    for (let i = 0; i < pixelArray.length; i++) {
+        let currentByte = pixelArray[i];
+        let lastBit = (currentByte & 1) ? 1 : 0;
+        pixelArray[i] = (currentByte >>> 1) | (lastBit << 8);
+    }
+    spriteTextBox.value = toHexString(pixelArray);
+    renderSpriteScreenFromText();
+}
+
 function updateSpriteArea(e) {
-    if (e.buttons == 1) {                           // If left click is pressed
+    if (e.buttons == 1) {                       // If left click is pressed
         const ctx = spriteEditorArea.getContext('2d');
         ctx.fillStyle = 'black';
         ctx.fillRect(Math.floor(e.offsetX / scalingFactor) * scalingFactor,
                     Math.floor(e.offsetY / scalingFactor) * scalingFactor,
                     scalingFactor,
                     scalingFactor);
+        // Turn the pixel on
         pixelArray[Math.floor(e.offsetY / scalingFactor)] |= (0b10000000 >>> Math.floor(e.offsetX / scalingFactor));
     }
 
@@ -51,29 +88,34 @@ function updateSpriteArea(e) {
                     Math.floor(e.offsetY / scalingFactor) * scalingFactor,
                     scalingFactor,
                     scalingFactor);
+        // Turn the pixel off
         pixelArray[Math.floor(e.offsetY / scalingFactor)] &= ~(0b10000000 >> Math.floor(e.offsetX / scalingFactor));
     }
 
-    spriteText.value = toHexString(pixelArray);
+    spriteTextBox.value = toHexString(pixelArray);
 }
 
 function renderSpriteScreenFromText() {
-    let valueToConvert = spriteText.value.split(' ');
-    for (let i = 0; i < valueToConvert.length; i++) {
-        valueToConvert[i] = parseInt(valueToConvert[i].trim());
-        pixelArray[i] = valueToConvert[i];
+    if (spriteTextBox.value == "") {
+        clearSpriteArea();
     }
-    
-    const ctx = spriteEditorArea.getContext("2d");
-    ctx.fillStyle = "#c4c4c4";
-    ctx.clearRect(0, 0, spriteEditorArea.width, spriteEditorArea.height);
 
-    for (let y = 0; y < valueToConvert.length; y++) {
-        for (let x = 0; x < 8; x++) {
-            if (valueToConvert[y] & (0b10000000 >>> x)) {
-                console.log(pixelArray[y] & (0b10000000 >>> x))
-                ctx.fillStyle = "black";
-                ctx.fillRect(x * scalingFactor, y * scalingFactor, scalingFactor, scalingFactor);
+    else {
+        let valueToConvert = spriteTextBox.value.split(',');
+        for (let i = 0; i < valueToConvert.length; i++) {
+            pixelArray[i] = parseInt(valueToConvert[i].trim());
+        }
+
+        const ctx = spriteEditorArea.getContext("2d");
+        ctx.fillStyle = "#c4c4c4";
+        ctx.clearRect(0, 0, spriteEditorArea.width, spriteEditorArea.height);
+
+        for (let y = 0; y < pixelArray.length; y++) {
+            for (let x = 0; x < 8; x++) {
+                if (pixelArray[y] & (0b10000000 >>> x)) {
+                    ctx.fillStyle = "black";
+                    ctx.fillRect(x * scalingFactor, y * scalingFactor, scalingFactor, scalingFactor);
+                }
             }
         }
     }
